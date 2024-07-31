ca9d28967cf86bfd38d035f22bea02c7101e5e9d
Denise Pingiori <pingiorid@gmail.com>
2024-07-31 16:27:29 +0200

    [upd] es finished

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
-<script setup>
+<script>
+//importo apptitle dal file della componente
+import AppTitle from './components/AppTitle.vue';
 
+//abilito all'utilizzo
+  export default {
+    components:{
+      AppTitle
+    }
+  }
 </script>
-
+  <AppTitle />
 <template>
-  
-  
+  <header>
+    
+  </header>
+  <main>
+    <h2>La mia Prima app con vite!</h2>
+  </main>
 </template>
 
 <style scoped>
